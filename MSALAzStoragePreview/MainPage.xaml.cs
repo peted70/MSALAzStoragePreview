faa3bdc11e424db0e20196a330024a5fe43b3404
peted70 <peted70@hotmail.com>
2019-03-04 18:14:40 +0000

    updated

--- MSALAzStoragePreview/MainPage.xaml.cs
+++ MSALAzStoragePreview/MainPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +26,43 @@ namespace MSALAzStoragePreview
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private PublicClientApplication pca;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async Task<string> AuthAsync()
+        {
+            // App registered in preview app registration blad on the portal
+            pca = new PublicClientApplication("ff165698-5680-441a-9935-e7e3b6f0ca4b");
+
+
+            // Need to have a scope of some kind here - what should it be? 
+            // Using user_impersonation for app registered as not v2 endpoint...
+            var res = await pca.AcquireTokenAsync(new string[] { "https://storage.azure.com/user_impersonation" });
+            return res.AccessToken;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var accessToken = await AuthAsync();
+            await CallContainerAsync(accessToken);
+        }
+
+        async Task CallContainerAsync(string token)
+        {
+            var http = new HttpClient();
+
+            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await http.GetAsync("https://msalazstorage.blob.core.windows.net/models?restype=container&comp=list");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return;
+            }
         }
     }
 }
